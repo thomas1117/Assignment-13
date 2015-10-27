1673b503fb9ed5c0c8b19386aad7eeeb4993a912
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-10-27 16:15:41 -0700

    added images

--- main.js
+++ main.js
@@ -8,18 +8,21 @@ $.ajax({
 	  method: 'get',
 	  dataType: 'json',
 	}).done(function(data){
-		
-		
 
+	
+	
 	var test= {data:data};
 
+	
+
+	
 	var templateString= $("#form").text();
 	var listHTML=Mustache.render(templateString, test);
 	$("#container").append(listHTML);
 
 });
 
+ 
 
-	
 
 });
